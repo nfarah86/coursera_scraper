26b8edb8cee927fdd56e45a54abd720996879e47
Nadine <nfarah86@gmail.com>
2015-02-21 16:52:35 -0800

    working on postg db

---
+++ coursera_scraper_app/settings.py
@@ -0,0 +1,10 @@
+BOT_NAME = 'coursera'
+SPIDER_MODULES = ['coursera_scraper_app.spiders']
+DATABASE = {
+    'drivername': 'postgres',
+    'host': 'localhost',
+    'port': '5432',
+    'username': 'nfarah86',
+    'password': 'dogs123',
+    'database': 'coursera_scraper_database'
+}
