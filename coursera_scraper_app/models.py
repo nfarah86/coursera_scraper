5ed0d2b77d2393eecd6be702cdd7b3cacfda05c1
Nadine <nfarah86@gmail.com>
2015-02-24 17:11:33 -0800

    rearranging my db

--- coursera_scraper_app/models.py
+++ coursera_scraper_app/models.py
@@ -1,20 +1,44 @@
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.engine.url import URL
-from sqlalchemy import create_engine, Column, Integer, String, Date
+from sqlalchemy import create_engine, Column, Integer, String
+import os
 import settings
 
 DeclarativeBase = declarative_base()
 
-def db_connect():
-    """ 
-    Database connects using db settings: import settings
-    sqlalchemy engine instance is returned
-    """ 
-    return create_engine(URL(**settings.DATABASE))
+# def db_connect():
+#     """ 
+#     Database connects using db settings: import settings
+#     sqlalchemy engine instance is returned
+#     """ 
+#     return create_engine(URL(**settings.DATABASE))
 
-def create_categories_table(engine):
+# def create_categories_table(engine):
 
-    DeclarativeBase.metadata.create_all(engine)
+#     DeclarativeBase.metadata.create_all(engine)
+
+ENGINE = create_engine(URL(**settings.DATABASE), echo=True)
+session = scoped_session(sessionmaker(bind=ENGINE,
+                                      autocommit = False,
+                                      autoflush = False))
+
+
+Base = declarative_base() #import Base
+Base.query = session.query_property
+
+
+def connect():
+    global ENGINE
+    global Session
+
+    ENGINE= create_engine(URL(**settings.DATABASE), echo=True)
+    Session = sessionmaker(bind = ENGINE)
+
+    return Session()
+
+def create_categories_db():
+    Base.metadata.create_all(bind=Engine)
 
 class Category(DeclarativeBase):
     """
@@ -28,6 +52,18 @@ class Category(DeclarativeBase):
     author = Column('author', String, nullable=False)
     start_date = Column('start_date', String, nullable=False)
     duration = Column('duration', String, nullable=False)
+
+
+################ creating instance of session, initalizing engine ################## 
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
 
 
 
--- coursera_scraper_app/piplines.py
+++ coursera_scraper_app/piplines.py
@@ -1,54 +1,36 @@
-from sqlalchemy.orm import sessionmaker
-from models import db_connect, Category, create_categories_table
 import csv
 
-
-class Coursera_Pipline(object):
+def process_item_to_db(self):
+    """ 
+    Saves categories scraped into db
     """
-     Coursera pipeline for storing
-     scraped items into db
-     """
-    
-    def __init__(self):
-        engine = db_connect()
-        create_categories_table(engine)
-        self.Session = sessionmaker(bind = engine,
-                                        autocommit = False,
-                                        autoflush = False)
+    with open("./spiders/data.tsv", 'r') as category_reader:
+        category_reader = csv.reader(category_reader, delimiter='\t')
+        for line in category_reader:
+            line[1] = line[1].replace('[', "").replace(']',"")
+            organizations, authors, titles, start_date, duration = line
+            #print (line)
 
-    def process_item_to_db(self):
-        """ 
-        Saves categories scraped into db
-        """
-        session = self.Session()
+            category = model.Category()
+            category.organizations = organizations
+            category.authors = authors
+            category.titles = titles
+            category.start_date = start_date
+            category.duration = duration
 
-        with open("./spiders/data.tsv", 'r') as category_reader:
-            category_reader = csv.reader(category_reader, delimiter='\t')
-            for line in category_reader:
-                line[1] = line[1].replace('[', "").replace(']',"")
-                organizations, authors, titles, start_date, duration = line
-                #print (line)
-
-                category = model.Category()
-                category.organizations = organizations
-                category.authors = authors
-                category.titles = titles
-                category.start_date = start_date
-                category.duration = duration
-
-        try:
-          session.add(category)
-          session.commit()
-        except:
-          session.rollback()
-          raise
-        finally:
-          session.close()
-        return category
+            try:
+              session.add(category)
+              session.commit()
+            except:
+              session.rollback()
+              raise
+            finally:
+              session.close()
+            #return category
 
 def main():
-    w_text = Coursera_Pipline()
-    print_text = w_text.process_item_to_db()
+    process_item_to_db()
+    connect = model.connect()
 
 if __name__ == '__main__':
-    main()
+    main(connect)
